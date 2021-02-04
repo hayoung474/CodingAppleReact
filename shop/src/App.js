8f2719a02855eac79e686a7f9f58339f599706fb
hayoungzzzzz <hayoung474@gmail.com>
2021-02-04 20:56:55 +0900

    2021.02.04 포맷을 위한 백업 part2 까지 완료

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Detail from './Detail'
+import axios from 'axios'
 
 import {Navbar ,Nav,NavDropdown,Jumbotron,Button} from 'react-bootstrap'
 
@@ -8,9 +9,11 @@ import React,{useState} from 'react'
 import Data from './data'
 
 import {Link,Route,Switch} from 'react-router-dom'
+import { useEffect } from 'react';
 
 function App() {
   let [shoes,shoes변경]= useState(Data);
+  let [재고,재고변경] = useState([10,11,12])
   return (
     <div className="App">
       <Navbar bg="light" expand="lg" className="">
@@ -18,8 +21,8 @@ function App() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link ><Link to="/">Home</Link></Nav.Link>
-            <Nav.Link ><Link to="/detail">Detail</Link></Nav.Link>
+            <Nav.Link as={Link}to="/">Home</Nav.Link>
+            <Nav.Link as={Link}to="/detail">Detail</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -51,18 +54,30 @@ function App() {
                 )
               })}
             </div>
+            <button className="btn btn-primary" onClick={()=>{
+              // 로딩중이라는  UI를 띄움
+              axios.get("https://codingapple1.github.io/shop/data2.json")
+              .then((res)=>{
+                // 로딩중이라는 UI 를 안보이게 처리
+                shoes변경([...shoes,...res.data])
+              })
+              .catch(()=>{
+                console.log("실패했어요")
+              });
+
+            }}>더보기</button>
           </div>
         </Route>
 
         <Route path="/detail/:id">
-          <Detail shoes={shoes}></Detail> {/* 다른 파일로 뺀 컴포넌트. Detail.js*/}
+          <Detail shoes={shoes} 재고={재고} 재고변경={재고변경}></Detail> {/* 다른 파일로 뺀 컴포넌트. Detail.js*/}
         </Route>
 
         <Route path="/:id">
           <div>아무거나 적었을 때</div>
         </Route>
       </Switch>
-      <button ><a href="http://www.naver.com">네이버</a></button>
+      {/* <button ><a href="http://www.naver.com">네이버</a></button> */}
       
 
 
--- shop/src/Detail.js
+++ shop/src/Detail.js
@@ -1,28 +1,58 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import styled from "styled-components";
-import './Detail.scss';
 
+import "./Detail.css";
 // 컴포넌트 임 !
 let 박스 = styled.div`
   padding: 20px;
-
 `;
-// props 문법 개능 .. 오 
+// props 문법 개능 .. 오
 let 제목 = styled.h4`
-    font-size:25px;
-    color:${props=>props.색상}
+  font-size: 25px;
+  color: ${(props) => props.색상};
 `;
 
 function Detail(props) {
   let history = useHistory();
   let { id } = useParams();
+  let [alert, alert변경] = useState(true);
+  let [inputData, inputData변경] = useState("");
+
+  // lifecycle 과 같은 hook. 컴포넌트가 mount 되었을 때 , update 되었을 때
+  useEffect(() => {
+    // detail 페이지 방문 후 alert 창이 2초 후에 사라지게 할 때 ?
+    let 타이머 = setTimeout(() => {
+      alert변경(false);
+    }, 2000);
+    // return function 어쩌구() {}; // detail 컴포넌트가 destoy 될 때 실행
+    return ()=>{clearTimeout(타이머)} // 타이머 제거
+  }, [alert]); // 특정 state 가 변경될 때만 실행해주세요 . 실행 조건
+  // [] 만 쓰면,  update시 실행되지 않음 mount 한 번만 실행
+
   let 찾은상품 = props.shoes.find(function (상품) {
     return 상품.id == id;
   });
   return (
     <div className="container">
-        <박스><제목 className="red" 색상={'blue'}>상세페이지</제목></박스>
+      <박스>
+        <제목 className="red" 색상="red">
+          Detail
+        </제목>
+      </박스>
+      {inputData}
+      {/* <input
+        onChange={(e) => {
+          inputData변경(e.target.value);
+        }}
+      /> */}
+      {/*input 에 무언가를 입력하면 재 렌더링(업데이트)이 발생함*/}
+      {alert === true ? (
+        <div className="my-alert">
+          <p> 재고가 얼마 남지 않았습니다</p>
+        </div>
+      ) : null}
       <div className="row">
         <div className="col-md-6">
           <img
@@ -34,7 +64,8 @@ function Detail(props) {
           <h4 className="pt-5">{찾은상품.title}</h4>
           <p>{찾은상품.content}</p>
           <p>{찾은상품.price}</p>
-          <button className="btn btn-danger">주문하기</button>
+          <Info 재고={props.재고}></Info>
+          <button className="btn btn-danger" onClick={()=>{props.재고변경([9,11,12])}}>주문하기</button>
           <button
             className="btn btn-danger ml-1"
             onClick={() => {
@@ -47,6 +78,12 @@ function Detail(props) {
       </div>
     </div>
   );
+}
+
+function Info(props){
+  return(
+    <p>재고:{props.재고[0]}</p>
+  )
 }
 
 export default Detail;
